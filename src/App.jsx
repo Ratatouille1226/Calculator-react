d995a1992e8a703fd9139ff0029d748b4a9a608d
Andrey <kotlyar.andrey03@mail.ru>
2024-12-15 19:02:23 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,53 @@ function App() {
   const [operand1, setOperand1] = useState('');
   const [operand2, setOperand2] = useState('');
   const [operator, setOperator] = useState('');
+  const [result, setResult] = useState(0);
+
+  const inputNumber = (newOperand) => {
+      if (operator) {
+        setOperand2(operand2 + newOperand);
+      } else {
+        setOperand1(operand1 + newOperand);
+      }
+  }
+
+  const inputOperator = (operator) => {
+    setOperator(operator);
+  }
+
+  const getResult = () => {
+    if (operator === '+') {
+      setResult(Number(operand1) + Number(operand2));
+    }
+    if (operator === '-') {
+      setResult(Number(operand1) - Number(operand2));
+    }
+  }
+
+  const reset = () => {
+      setOperand1('');
+      setOperand2('');
+      setOperator('');
+      setResult('');
+  }
 
   return (
     <div className={styles.app}>
       <div className={styles.calculator}>
         <div className={styles.field}>
-          <div className={styles.result}>{0}</div>
+          <div className={styles.result}>{result ? result : `${operand1}${operator}${operand2}`}</div>
         </div>
         <div className={styles.commands}>
           <div className={styles.numbers}>
             {NUMS.map(item => (
-              <button key={+item} className={styles.numbersButton}>{item}</button>
+              <button key={+item} className={styles.numbersButton} onClick={() => inputNumber(item)}>{item}</button>
             ))}
           </div>
           <div className={styles.operators}>
-              <button>+</button>
-              <button>-</button>
-              <button>=</button>
-              <button>c</button>
+              <button onClick={() => inputOperator('+')}>+</button>
+              <button onClick={() => inputOperator('-')}>-</button>
+              <button onClick={getResult}>=</button>
+              <button onClick={reset}>c</button>
           </div>
         </div>
       </div>
