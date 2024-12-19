7322168b5237a9760aaf3b767d40dd390e7ab86b
Andrey <kotlyar.andrey03@mail.ru>
2024-12-19 14:23:36 +0300

    Новые правки

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,64 @@
 import { useState } from 'react';
 import styles from './app.module.css';
 
+const NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
 function App() {
-  const NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-  const OPERATORS = ['+', '-', '=', 'C'];
+  const [operand1, setOperand1] = useState('');
+  const [operand2, setOperand2] = useState('');
+  const [operator, setOperator] = useState('');
+  const [result, setResult] = useState(null);
+
+  const onClickNumber = (newOperand) => {
+      if (operator) {
+        setOperand2((prev) => prev + newOperand);
+      } else {
+        setOperand1((prev) => prev + newOperand);
+      }
+  }
+
+  const onClickOperator = (operator) => {
+    setOperator(operator);
+  }
+
+  const onClickResult = () => {
+    let result = '';
+    switch (operator) {
+      case '+':
+        result = Number(operand1) + Number(operand2);
+        break; 
+      case '-':
+        result = Number(operand1) - Number(operand2);
+        break; 
+    }
+
+    setResult(result);
+  }
   
-  const [calc, setCalc] = useState('');
-  const [isCalcResult, setIsCalcResult] = useState(false);
-
-  const performTheOperation = (num) => {
-    if (num === '=') {
-      setCalc(eval(calc));
-      setIsCalcResult(true);
-    } else {
-      setIsCalcResult(false);
-      setCalc((prev) => prev + num);
-    }
-
-    if (num === 'C') {
-      setCalc('');
-      setIsCalcResult(false);
-    }
+  const reset = () => {
+      setOperand1('');
+      setOperand2('');
+      setOperator('');
+      setResult(null);
   }
 
   return (
     <div className={styles.app}>
       <div className={styles.calculator}>
         <div className={styles.field}>
-          <div className={isCalcResult ? styles.resultCalc : styles.result}>{calc}</div>
+          <div className={styles.result}>{result !== null ? result : `${operand1}${operator}${operand2}`}</div>
         </div>
         <div className={styles.commands}>
           <div className={styles.numbers}>
-            {
-            NUMS.map(button => (
-              <button 
-                  key={+button} 
-                  className={styles.numbersButton} 
-                  onClick={() => {performTheOperation(button)}}
-              >
-                {button}
-              </button>
-            ))
-            }
+            {NUMS.map(item => (
+              <button key={+item} className={styles.numbersButton} onClick={() => onClickNumber(item)}>{item}</button>
+            ))}
           </div>
           <div className={styles.operators}>
-            {
-            OPERATORS.map((operator, index) => (
-              <button 
-                  key={index} 
-                  onClick={() => {performTheOperation(operator)}} 
-              >
-                {operator}
-              </button>
-            ))
-            }
+            <button onClick={() => onClickOperator('+')}>+</button>
+            <button onClick={() => onClickOperator('-')}>-</button>
+            <button onClick={onClickResult}>=</button>
+            <button onClick={reset}>c</button>
           </div>
         </div>
       </div>
